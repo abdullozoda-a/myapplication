fb9db8db256da3fcdabcf539cea4a4d6c5483a4c
abdullozoda_oo8 <abdullozodaanush8@gmail.com>
2024-11-24 14:55:40 +0500

    create project

---
+++ project6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace project6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var result = a > 100 && a < 999 && a % 2 != 0;
+            Console.WriteLine(result);
+        }
+    }
+}
